82dfd6a8047b3ee9bb0dc608a068789eb0fae648
Mohammed Joof <MohammedJoof@outlook.com>
2022-12-27 15:59:35 +0000

    Added deck and card dealing

---
+++ CeroGame.GamePresentation/Components/Deck.razor.cs
@@ -0,0 +1,27 @@
+﻿using CeroGame.GameService.Models;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CeroGame.GamePresentation.Components
+{
+    public partial class Deck
+    {
+
+        [Parameter]
+        public int CardsPerRow { get; set; } = 7;
+        [Parameter]
+        public bool Hidden { get; set; } = true;
+        private List<CardModel> _cards { get; set; } = new();
+        
+        [Parameter]
+        public List<CardModel> Cards
+        {
+            get => _cards;
+            set => _cards = value.OrderBy(x=> x.Colour).ThenBy(x=> x.Number).ToList();
+        }
+    }
+}
